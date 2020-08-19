f45ba242b232d53fa3ceb137c27deed1366a0e51
Iksent <skiym@yandex.ru>
2020-08-19 10:46:57 +0500

    Initial Commit with fixes

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import View from "../components/View";
+import React from "react"
+
+const NotFound = () => (
+    <View title="Not Found">
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </View>
+)
+
+export default NotFound
